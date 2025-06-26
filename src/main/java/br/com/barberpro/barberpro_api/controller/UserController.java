6238f72014693acfef944b78e7f7387fe54ce0ee
Roberto <robertoujesus@hotmail.com>
2025-06-26 14:13:20 -0300

    criado classes UserController e UserService

---
+++ src/main/java/br/com/barberpro/barberpro_api/controller/UserController.java
@@ -0,0 +1,49 @@
+package br.com.barberpro.barberpro_api.controller;
+
+import br.com.barberpro.barberpro_api.entity.User;
+import br.com.barberpro.barberpro_api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> listAll() {
+        return userService.findAll();
+    }
+
+   import org.springframework.http.ResponseEntity;
+
+// ...
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById(@PathVariable Long id) {
+        return userService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public User update(@PathVariable Long id, @RequestBody User user) {
+        user.setId(id);
+        return userService.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        userService.delete(id);
+    }
+}
